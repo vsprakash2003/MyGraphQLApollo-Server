92b8cc739aea9b812199504e9296620187252b07
Mycents <prakashsubramani@hotmail.com>
2018-12-11 14:48:56 -0500

    update schema to include page, offset, limits in resolvers

--- env/schema/schema.py
+++ env/schema/schema.py
@@ -1,10 +1,9 @@
 from os import path
 import graphene
-from graphene import relay
-from graphene_sqlalchemy import SQLAlchemyConnectionField
+from model.department import Department as DepartmentModel
 from .query_types import Department, Employee
 from .mutations import AddDepartment
-from logging.config import fileConfig
+
 import logging
 
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'myapplog.log')
@@ -13,21 +12,59 @@ logger = logging.getLogger()
 
 
 class Query(graphene.ObjectType):
-    all_departments = graphene.List(Department)
+    all_departments = graphene.List(lambda: graphene.List(Department), 
+                                    department_id=graphene.String(),
+                                    offset=graphene.Int(),
+                                    limit=graphene.Int(),
+                                    sort_order=graphene.String(),
+                                    sort_field=graphene.String())
     all_employees = graphene.List(Employee)
-    department_by_id = graphene.Field(lambda: graphene.List(Department), 
-                                      department_id=graphene.String())
+    department_by_id = graphene.Field(lambda: graphene.List(Department),
+                                      department_id=graphene.String(),
+                                      offset=graphene.Int(),
+                                      limit=graphene.Int(),
+                                      sort_order=graphene.String(),
+                                      sort_field=graphene.String())
 
-    def resolve_all_departments(self, info):
+    def resolve_all_departments(self, info, department_id=None, offset=None,
+                                limit=None, sort_order='asc',
+                                sort_field='deptid'):
         logger.info("Inside all_departments resolver")
         query = Department.get_query(info)
+
+        if sort_field and sort_order:
+            if sort_field == 'deptid':
+                if sort_order == 'asc':
+                    query = query.order_by(DepartmentModel.department_id.asc())
+                elif sort_order == 'desc':
+                    query = query.order_by(DepartmentModel.department_id.desc()
+                                           )
+
+        if limit:
+            query = query.limit(limit).offset((offset - 1) * limit)
+
         return query.all()
 
-    def resolve_department_by_id(self, info, department_id=None):
+    def resolve_department_by_id(self, info, department_id=None, offset=None,
+                                 limit=None, sort_order='asc',
+                                 sort_field='deptid'):
         logger.info("Inside department_by_id resolver")
         query = Department.get_query(info)
+        
         if department_id:
             query = query.filter_by(department_id=department_id)
+
+        if sort_field and sort_order:
+            if sort_field == 'deptid':
+                if sort_order == 'asc':
+                    query = query.order_by(DepartmentModel.department_id.asc())
+                elif sort_order == 'desc':
+                    query = query.order_by(DepartmentModel.department_id.desc()
+                                           )
+
+        if limit:
+            query = query.limit(limit).offset((offset - 1) * limit)
+        
         return query.all()
 
     def resolve_all_employees(self, info, Employee_id=None):
