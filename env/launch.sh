6e897d3e2ed10ee74d75d6c2c8bacf705a02e96d
Mycents <prakashsubramani@hotmail.com>
2019-01-10 16:24:51 -0500

    Update launch to remove duplicate docker-compose commands

--- env/launch.sh
+++ env/launch.sh
@@ -6,16 +6,10 @@
 cd $(dirname $0)
 
 # Rebuild the image (required if dependencies change)
-docker-compose -f docker-compose.yml build
-
-# Build and start docker
-docker-compose -f docker-compose.yml up -d
-
-# Give the db a sec to spin up
-sleep 5
+docker-compose -p Mydocker -f docker-compose.yml build
 
 # Run npm scripts to build the spec and migrate the database
-docker-compose -f docker-compose.yml -p Mydocker run --rm -d --entrypoint="sh -c \"alembic upgrade head\""
+docker-compose -p Mydocker -f docker-compose.yml run --rm -d --entrypoint="sh -c \"alembic upgrade head\""
 
 # Start web and server
 docker-compose -f docker-compose.yml up -d
